8d746023bc0cea394502526fd51b1009311b373b
hasthik <hasthikdjathan644@gmail.com>
2021-09-22 22:45:54 +0530

    creating navbar and table head

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Covid Dashboard</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Data-Table-1.css">
+    <link rel="stylesheet" href="assets/css/Data-Table.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/sticky-dark-top-nav.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navbar-fixed-top navigation-clean-button">
+        <div class="container">
+            <div><a class="navbar-brand" href="#"><span>Covid19 Dashboard India</span> </a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button></div>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav nav-right">
+                    <li class="nav-item"><a class="nav-link active" href="index.html" style="color: #888888;">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" style="color: #888888;">Graphs</a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="faq.html" style="color: #888888;">World</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html" style="color: #888888;">Dev Info</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav><table id="example" class="table table-striped table-bordered" cellspacing="0" align="center" width="100%">
+    <thead>
+  <tr>
+    <th>Last Updated Time</th>
+    <th>State</th>
+    <th>Confirmed</th>
+    <th>Active</th>
+    <th>Recovered</th>
+    <th>Deaths</th>
+  </tr>
+    </thead>
+  
+
+</body>
+</html>
